0c4040193e2c13f8899336302a01a9e4429a0b7c
Jamo112 <a1925902@adelaide.edu.au>
2024-08-21 14:29:30 +0930

    Add files via upload

---
+++ Unit.cpp
@@ -0,0 +1,21 @@
+#include "Unit.h"
+
+Unit:: Unit() : unit_val(0), num_beds(0), unit_size(0) {};
+
+Unit::Unit(int unit_val, int num_beds, double unit_size){
+this-> unit_val = unit_val;
+this-> num_beds = num_beds;
+this-> unit_size = unit_size;
+};
+
+int Unit::get_Num_Bedrooms(){
+    return num_beds;
+}
+
+int Unit::get_Value(){
+return unit_val;
+}
+
+double Unit::get_Area(){
+return unit_size;
+}
---
+++ main-1-2.cpp
@@ -0,0 +1,18 @@
+#include "Unit.h"
+#include "ApartmentBuilding.h"
+#include <iostream>
+
+
+int main() {
+    ApartmentBuilding m1;
+    std::cout << "Default Value: " << m1.get_Capacity() << ", Default Bedrooms " << m1.get_Current_Number_of_Units() << ", Default size " << m1.get_Contents() << std::endl;
+
+    ApartmentBuilding m2(10);
+    std::cout << "Changed Value: " << m2.get_Capacity() << ", changed Bedrooms " << m2.get_Current_Number_of_Units() << ", Changed size " << m2.get_Contents() << std::endl;
+
+
+    return 0;
+}
+
+
+
