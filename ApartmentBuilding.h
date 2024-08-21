1c9a623eb0e9879112a04a6a51eaaecfb2f5e603
Jamo112 <a1925902@adelaide.edu.au>
2024-08-21 14:14:47 +0930

    Add files via upload

---
+++ ApartmentBuilding.h
@@ -0,0 +1,25 @@
+#ifndef APARTMENT_H
+#define APARTMENT_H
+#include "Unit.h"
+
+class ApartmentBuilding {
+private:
+int Capacity;
+int Current_Nom_Units;
+Unit* unit;
+
+
+public:
+ApartmentBuilding();
+ApartmentBuilding(int Capacity);   
+int get_capacity();
+int get_Current_Number_of_Units();
+Unit * get_Contents();
+bool add_Unit(Unit unit);
+~ApartmentBuilding();
+};
+
+
+
+
+#endif
---
+++ Unit.h
@@ -0,0 +1,21 @@
+#ifndef UNIT_H
+#define UNIT_H
+
+class Unit {
+private:
+int unit_val;
+int num_beds;
+double unit_size;
+
+public:
+Unit();
+Unit(int unit_val, int num_beds, double unit_size);   
+int get_Num_Bedrooms();
+int get_Value();
+double get_Area();
+};
+
+
+
+
+#endif
